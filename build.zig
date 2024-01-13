09392b649ff48a7a9bfe0491a49c3f1075a380c0
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2024-01-13 19:56:17 +1000

    build updates

--- build.zig
+++ build.zig
@@ -24,18 +24,19 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the demo app");
     run_step.dependOn(&run_cmd.step);
 
-    const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/tests.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    tests.root_module.addImport("datastor", datastor_module);
-    const run_test = b.addRunArtifact(tests);
-    run_test.has_side_effects = true;
+    // TODO - add tests
+    // const tests = b.addTest(.{
+    //     .root_source_file = .{ .path = "src/tests.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // tests.root_module.addImport("datastor", datastor_module);
+    // const run_test = b.addRunArtifact(tests);
+    // run_test.has_side_effects = true;
 
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_test.step);
+    // const test_step = b.step("test", "Run tests");
+    // test_step.dependOn(&run_test.step);
 }
