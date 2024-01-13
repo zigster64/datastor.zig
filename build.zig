07f5e1f0a4d576cb05e07cae16c7abe99e779d4a
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2024-01-13 19:50:21 +1000

    bring up to zig 0.12 build system

--- build.zig
+++ build.zig
@@ -3,26 +3,39 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const dep_opts = .{ .target = target, .optimize = optimize };
+    _ = dep_opts;
 
     const datastor_module = b.addModule("datastor", .{
-        .source_file = .{ .path = "src/datastor.zig" },
+        .root_source_file = .{ .path = "src/datastor.zig" },
     });
 
-    // setup executable
-    const example_exe = b.addExecutable(.{
-        .name = "datastor_example",
+    const exe = b.addExecutable(.{
+        .name = "datastor.zig demo",
         .root_source_file = .{ .path = "example/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    example_exe.addModule("datastor", datastor_module);
-    b.installArtifact(example_exe);
+    exe.root_module.addImport("datastor", datastor_module);
+    b.installArtifact(exe);
 
-    const run_example = b.addRunArtifact(example_exe);
-    run_example.step.dependOn(b.getInstallStep());
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
-        run_example.addArgs(args);
+        run_cmd.addArgs(args);
     }
-    const run_step = b.step("run", "Run the example app");
-    run_step.dependOn(&run_example.step);
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/tests.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    tests.root_module.addImport("datastor", datastor_module);
+    const run_test = b.addRunArtifact(tests);
+    run_test.has_side_effects = true;
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_test.step);
 }
